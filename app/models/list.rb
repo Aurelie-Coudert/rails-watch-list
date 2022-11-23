5591cf1ccaf153da93b45016e801773d6829a380
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-11-23 23:11:42 +0100

    4 errors left to solve

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
-  has_many :movies
-  has_many :bookmarks
+  has_many :movies, through: :bookmarks, source: :movie
+  has_many :bookmarks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
